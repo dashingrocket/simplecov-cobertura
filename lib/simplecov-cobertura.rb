7f144022b88a02ba874c32da58ea85a927ec79b3
Jesse Bowes <jessebowes@noragh.com>
2016-10-04 18:52:15 -0600

    Bump dev versions

--- lib/simplecov-cobertura.rb
+++ lib/simplecov-cobertura.rb
@@ -4,7 +4,7 @@ require 'rexml/element'
 module SimpleCov
   module Formatter
     class CoberturaFormatter
-      VERSION = '1.2.0'
+      VERSION = '1.2.1-dev'
 
       RESULT_FILE_NAME = 'coverage.xml'
       DTD_URL = 'http://cobertura.sourceforge.net/xml/coverage-04.dtd'
