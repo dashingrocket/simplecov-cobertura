6c8f4803982f2094b7daeb1607c9ac854bad920c
Jesse Bowes <jessebs@users.noreply.github.com>
2021-11-24 13:25:55 -0800

    Merge prep #20 (#23)
    * [+] branch coverage
    
    * Change the calculation of condition-coverage considering the number of branches and hits per line
    
    * [fix] tests
    
    * Create conditional-coverage from branch coverage
    
    * Merge master for #22
    
    Co-authored-by: Viktor Sych <skcc321@gmail.com>
    Co-authored-by: Emílio S. do Carmo <emilio2hd@gmail.com>

--- lib/simplecov-cobertura.rb
+++ lib/simplecov-cobertura.rb
@@ -28,105 +28,144 @@ module SimpleCov
       end
 
       private
-      def result_to_xml(result)
-        doc = REXML::Document.new set_xml_head
-        doc.context[:attribute_quote] = :quote
-        doc.add_element REXML::Element.new('coverage')
-        coverage = doc.root
 
-        set_coverage_attributes(coverage, result)
+        def result_to_xml(result)
+          doc = REXML::Document.new set_xml_head
+          doc.context[:attribute_quote] = :quote
+          doc.add_element REXML::Element.new('coverage')
+          coverage = doc.root
 
-        coverage.add_element(sources = REXML::Element.new('sources'))
-        sources.add_element(source = REXML::Element.new('source'))
-        source.text = SimpleCov.root
+          set_coverage_attributes(coverage, result)
 
-        coverage.add_element(packages = REXML::Element.new('packages'))
+          coverage.add_element(sources = REXML::Element.new('sources'))
+          sources.add_element(source = REXML::Element.new('source'))
+          source.text = SimpleCov.root
 
-        if result.groups.empty?
-          groups = {File.basename(SimpleCov.root) => result.files}
-        else
-          groups = result.groups
-        end
+          coverage.add_element(packages = REXML::Element.new('packages'))
 
-        groups.map do |name, files|
-          next if files.empty?
-          packages.add_element(package = REXML::Element.new('package'))
-          set_package_attributes(package, name, files)
+          if result.groups.empty?
+            groups = {File.basename(SimpleCov.root) => result.files}
+          else
+            groups = result.groups
+          end
 
-          package.add_element(classes = REXML::Element.new('classes'))
+          groups.each do |name, files|
+            next if files.empty?
+            packages.add_element(package = REXML::Element.new('package'))
+            set_package_attributes(package, name, files)
 
-          files.each do |file|
-            classes.add_element(class_ = REXML::Element.new('class'))
-            set_class_attributes(class_, file)
+            package.add_element(classes = REXML::Element.new('classes'))
 
-            class_.add_element(REXML::Element.new('methods'))
-            class_.add_element(lines = REXML::Element.new('lines'))
+            files.each do |file|
+              classes.add_element(class_ = REXML::Element.new('class'))
+              set_class_attributes(class_, file)
 
-            file.lines.each do |file_line|
-              if file_line.covered? || file_line.missed?
-                lines.add_element(line = REXML::Element.new('line'))
-                set_line_attributes(line, file_line)
+              class_.add_element(REXML::Element.new('methods'))
+              class_.add_element(lines = REXML::Element.new('lines'))
+
+              branched_lines = file.branches.map(&:start_line)
+              branched_lines_covered = file.covered_branches.map(&:start_line)
+
+              file.lines.each do |file_line|
+                if file_line.covered? || file_line.missed?
+                  lines.add_element(line = REXML::Element.new('line'))
+                  set_line_attributes(line, file_line)
+                  set_branch_attributes(line, file_line, branched_lines, branched_lines_covered) if SimpleCov.branch_coverage?
+                end
               end
             end
           end
+
+          doc
         end
 
-        doc
-      end
+        def set_coverage_attributes(coverage, result)
+          ls = result.coverage_statistics[:line]
+          bs = result.coverage_statistics[:branch]
 
-      def set_coverage_attributes(coverage, result)
-        coverage.attributes['line-rate'] = (result.covered_percent/100).round(2).to_s
-        coverage.attributes['branch-rate'] = '0'
-        coverage.attributes['lines-covered'] = result.covered_lines.to_s
-        coverage.attributes['lines-valid'] = (result.covered_lines + result.missed_lines).to_s
-        coverage.attributes['branches-covered'] = '0'
-        coverage.attributes['branches-valid'] = '0'
-        coverage.attributes['branch-rate'] = '0'
-        coverage.attributes['complexity'] = '0'
-        coverage.attributes['version'] = '0'
-        coverage.attributes['timestamp'] = Time.now.to_i.to_s
-      end
+          coverage.attributes['line-rate'] = extract_rate(ls.percent)
+          coverage.attributes['lines-covered'] = ls.covered.to_s.to_s
+          coverage.attributes['lines-valid'] = ls.total.to_s.to_s
+          if SimpleCov.branch_coverage?
+            coverage.attributes['branches-covered'] = bs.covered.to_s
+            coverage.attributes['branches-valid'] = bs.total.to_s
+            coverage.attributes['branch-rate'] = extract_rate(bs.percent)
+          end
+          coverage.attributes['complexity'] = '0'
+          coverage.attributes['version'] = '0'
+          coverage.attributes['timestamp'] = Time.now.to_i.to_s
+        end
 
-      def set_package_attributes(package, name, result)
-        package.attributes['name'] = name
-        package.attributes['line-rate'] = (result.covered_percent/100).round(2).to_s
-        package.attributes['branch-rate'] = '0'
-        package.attributes['complexity'] = '0'
-      end
+        def set_package_attributes(package, name, result)
+          ls = result.coverage_statistics[:line]
+          bs = result.coverage_statistics[:branch]
 
-      def set_class_attributes(class_, file)
-        filename = file.filename
-        class_.attributes['name'] = File.basename(filename, '.*')
-        class_.attributes['filename'] = resolve_filename(filename)
-        class_.attributes['line-rate'] = (file.covered_percent/100).round(2).to_s
-        class_.attributes['branch-rate'] = '0'
-        class_.attributes['complexity'] = '0'
-      end
+          package.attributes['name'] = name
+          package.attributes['line-rate'] = extract_rate(ls.percent)
+          if SimpleCov.branch_coverage?
+            package.attributes['branch-rate'] = extract_rate(bs.percent)
+          end
+          package.attributes['complexity'] = '0'
+        end
 
-      def set_line_attributes(line, file_line)
-        line.attributes['number'] = file_line.line_number.to_s
-        line.attributes['branch'] = 'false'
-        line.attributes['hits'] = file_line.coverage.to_s
-      end
+        def set_class_attributes(class_, file)
+          ls = file.coverage_statistics[:line]
+          bs = file.coverage_statistics[:branch]
 
-      def set_xml_head(lines=[])
-        lines << "<?xml version=\"1.0\"?>"
-        lines << "<!DOCTYPE coverage SYSTEM \"#{DTD_URL}\">"
-        lines << "<!-- Generated by simplecov-cobertura version #{VERSION} (https://github.com/dashingrocket/simplecov-cobertura) -->"
-        lines.join("\n")
-      end
+          filename = file.filename
+          class_.attributes['name'] = File.basename(filename, '.*')
+          class_.attributes['filename'] = resolve_filename(filename)
+          class_.attributes['line-rate'] = extract_rate(ls.percent)
+          if SimpleCov.branch_coverage?
+            class_.attributes['branch-rate'] = extract_rate(bs.percent)
+          end
+          class_.attributes['complexity'] = '0'
+        end
 
-      def coverage_output(result)
-        "#{result.covered_lines} / #{result.covered_lines + result.missed_lines} LOC (#{result.covered_percent.round(2)}%) covered."
-      end
+        def set_line_attributes(line, file_line)
+          line.attributes['number'] = file_line.line_number.to_s
+          line.attributes['hits'] = file_line.coverage.to_s
+        end
 
-      def resolve_filename(filename)
-        Pathname.new(filename).relative_path_from(project_root).to_s
-      end
+        def set_branch_attributes(line, file_line, branched_lines, branched_lines_covered)
+          if branched_lines.include? file_line.number
+            pct_coverage, branches_covered = branched_lines_covered.include?(file_line.number) ? [100, '1/1'] : [0, '0/1']
+            line.attributes['branch'] = 'true'
+            line.attributes['condition-coverage'] = "#{pct_coverage}% (#{branches_covered})"
+          else
+            line.attributes['branch'] = 'false'
+          end
+        end
 
-      def project_root
-        @project_root ||= Pathname.new(SimpleCov.root)
-      end
+        def set_xml_head(lines=[])
+          lines << "<?xml version=\"1.0\"?>"
+          lines << "<!DOCTYPE coverage SYSTEM \"#{DTD_URL}\">"
+          lines << "<!-- Generated by simplecov-cobertura version #{VERSION} (https://github.com/dashingrocket/simplecov-cobertura) -->"
+          lines.join("\n")
+        end
+
+        def coverage_output(result)
+          ls = result.coverage_statistics[:line]
+          bs = result.coverage_statistics[:branch]
+
+          if SimpleCov.branch_coverage?
+            "%d / %d LOC (%.2f%%) covered; %d / %d BC (%.2f%%) covered" % [ls.covered, ls.total, ls.percent, bs.covered, bs.total, bs.percent]
+          else
+            "%d / %d LOC (%.2f%%) covered" % [ls.covered, ls.total, ls.percent]
+          end
+        end
+
+        def resolve_filename(filename)
+          Pathname.new(filename).relative_path_from(project_root).to_s
+        end
+
+        def extract_rate(percent)
+          (percent / 100).round(2).to_s
+        end
+
+        def project_root
+          @project_root ||= Pathname.new(SimpleCov.root)
+        end
     end
   end
 end
